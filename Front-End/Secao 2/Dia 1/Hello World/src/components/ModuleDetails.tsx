4b01cc8718f446a38a0b0534f3683d19d081f59f
Janiele Vilar <vilarjaniele@gmail.com>
2024-01-23 10:17:07 -0300

    Exercicio de componetizacao com React

---
+++ Front-End/Secao 2/Dia 1/Hello World/src/components/ModuleDetails.tsx
@@ -0,0 +1,17 @@
+function ModuleDetails() {
+  return (
+    <>
+      <div>
+        <h2>Módulos</h2>
+        <ul>
+          <li>Fundamentos</li>
+          <li>Front End</li>
+          <li>Back End</li>
+          <li>Ciência da Computação</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default ModuleDetails
